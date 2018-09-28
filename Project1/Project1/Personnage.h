7362d85c943c55a022d3c2f4a8417c937c28823b
Florian <flolevy33@gmail.com>
2018-09-28 03:22:42 -0400

    Personnage attributs

--- Project1/Project1/Personnage.h
+++ Project1/Project1/Personnage.h
@@ -1,8 +1,43 @@
 #pragma once
-class Personnage
-{
-public:
-	Personnage();
-	~Personnage();
-};
 
+#ifndef PERSONNAGE_H_
+#define PERSONNAGE_H_
+#include <iostream>
+using namespace std;
+
+namespace JeuBaston {
+	class Personnage
+	{
+	public:
+		Personnage();
+		Personnage(string nom);
+		Personnage(string nom, int age);
+
+		virtual ~Personnage();
+
+		int getAge() const {
+			return age;
+		}
+
+		void setAge(int age) {
+			this->age = age;
+		}
+
+		const string& getNom() const {
+			return nom;
+		}
+
+		void setNom(const string& nom) {
+			this->nom = nom;
+		}
+
+	protected:
+		string nom;
+		int age;
+
+
+	};
+
+
+
+}
