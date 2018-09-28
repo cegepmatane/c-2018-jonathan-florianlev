1c0d42584b7970733186ce3c4a024fe9667533c1
Florian <flolevy33@gmail.com>
2018-09-28 03:31:05 -0400

    fix

--- Project1/Project1/Personnage.cpp
+++ Project1/Project1/Personnage.cpp
@@ -1,12 +1,25 @@
 #include "Personnage.h"
 
 
+namespace JeuBaston {
 
-Personnage::Personnage()
-{
-}
+	Personnage::Personnage()
+	{
+		this->age = 0;
+		this->nom = "Inconnu";
+	}
 
+	Personnage::Personnage(string nom)
+	{
+		this->age = 0;
+		this->nom = nom;
+	}
+	Personnage::Personnage(string nom, int age) {
+		this->age = age;
+		this->nom = nom;
+	}
 
-Personnage::~Personnage()
-{
+	Personnage::~Personnage() {
+	}
+
 }
