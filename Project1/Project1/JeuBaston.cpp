c2288cf3611fade7daea6764780ad3992cb5666c
Florian <flolevy33@gmail.com>
2018-10-21 04:16:44 -0400

    export personnages

--- Project1/Project1/JeuBaston.cpp
+++ Project1/Project1/JeuBaston.cpp
@@ -7,6 +7,9 @@
 #include "Marteau.h"
 #include "Magicien.h"
 #include "Guerrier.h"
+#include <conio.h>;
+#include <chrono>;
+#include <thread>;
 
 
 using namespace std;
@@ -22,19 +25,83 @@ int main() {
 
 	Personnage* magicienGlace; // instance du pointeur vers un objet personnage
 	magicienGlace = new Magicien("Gandalf", 100);
-	//cout << magicienGlace->exporter() << endl; // Quelle fonction ??? Magicien::exporter
+	cout << magicienGlace->exporter() ;
 	magicienGlace->ajouterArme(bouleGlace);
 
 	Personnage* guerrier;
 	guerrier = new Guerrier("soldat", 100);
 	guerrier->ajouterArme(epee);
 
-	delete bouleGlace;
-	delete epee;
-	delete magicienGlace;
-	delete guerrier;
-	
+	ofstream fichierBaston;
+	fichierBaston.open("../data/baston.xml");
+	fichierBaston << "<baston>" << magicienGlace->exporter() << guerrier->exporter() << "</baston>";
+	fichierBaston.close();
 
-	return 0;
-	
+	ifstream fichierPersonnages;
+	fichierPersonnages.open("../data/baston.csv");
+	string ligne;
+	//getline(fichierPersonnages, ligne);
+	//cout << "Première ligne : " << ligne;
+	unsigned int dernierePosition = 0;
+	unsigned int positionPointVirgule = 0;
+	while (!fichierPersonnages.eof())
+	{
+		getline(fichierPersonnages, ligne);
+		cout << "Ligne : " << ligne << endl;
+		//int trouvaille = ligne.find(";", 6);
+		//cout << "test trouvaille " << trouvaille;
+		dernierePosition = 0;
+		do
+		{
+			positionPointVirgule = ligne.find(";", dernierePosition); // find retourne la position du prochain ;
+			//cout << " positionPointVirgule : " << positionPointVirgule;
+			string valeur = ligne.substr(dernierePosition, positionPointVirgule - dernierePosition); // substr prend la position de départ + le nombre de char
+			//cout << " position : " << positionPointVirgule;
+			cout << " valeur : " << valeur;
+			//cout << " positionPointVirgule avant +1: " << positionPointVirgule;
+			dernierePosition = positionPointVirgule + 1;
+			//cout << " derniere position apres +1: " << dernierePosition;
+			//cout << endl;
+		}
+		//while(ligne.length() > dernierePosition && dernierePosition > 0);
+		//while(string::npos != dernierePosition);
+		while (0 != dernierePosition); // 0 car -1 de find +1 = 0
+		cout << endl;
+
+		delete bouleGlace;
+		delete epee;
+		delete magicienGlace;
+		delete guerrier;
+
+		//Boucle jeu
+		int tour = 0;
+		int touche;
+		bool gameIsRunning = true;
+		while (gameIsRunning)
+		{
+			// PREMIER PROBLEME-SOLUTION : dormir sans occuper les ressources
+			this_thread::sleep_for(chrono::milliseconds(1000 / 60));
+			// SECOND PROBLEME-SOLUTION : lire les entrées sans bloquer la boucle
+			if (_kbhit())
+			{
+				touche = _getch();
+				cout << "touche " << touche << " " << tour << endl;
+				switch (touche)
+				{
+				case 'q':
+					gameIsRunning = false;
+					break;
+				}
+			}
+			while (_kbhit()) _getch();
+			//cout << "tour " << tour << endl;
+			tour++;
+		}
+
+		//system("pause");
+		return 0;
+
+
+
+	}
 }
